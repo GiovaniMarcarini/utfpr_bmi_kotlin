27e1171095ec75c0105af462a4ee6be8316f3f26
GiovaniMarcarini <inf_giovani@hotmail.com>
2024-09-12 12:54:55 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/utfpr/utfpr_bmi_kotlin/MainActivity.kt
@@ -0,0 +1,47 @@
+package br.edu.utfpr.utfpr_bmi_kotlin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.edu.utfpr.utfpr_bmi_kotlin.ui.theme.Utfpr_bmi_kotlinTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Utfpr_bmi_kotlinTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Utfpr_bmi_kotlinTheme {
+        Greeting("Android")
+    }
+}
